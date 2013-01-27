a8a1b093c3d09da128ccd22d5d73e40e5b7da062
Byron <contact@byronlevene.net>
2013-01-27 17:26:13 +0900

    custom login CSS hook

--- functions.php
+++ functions.php
@@ -1,1 +1,6 @@
 /* FUNCTIONS.PHP */
+
+function my_login_stylesheet() { ?>
+    <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo( 'stylesheet_directory' ) . '/style-login.css'; ?>" type="text/css" media="all" />
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
