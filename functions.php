58de4e940bbe5430d7b05f5701ec4b05b310cda3
Byron <contact@byronlevene.net>
2013-01-27 17:37:19 +0900

    syntax??

--- functions.php
+++ functions.php
@@ -3,4 +3,5 @@
 function my_login_stylesheet() { ?>
     <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo( 'stylesheet_directory' ) . '/style-login.css'; ?>" type="text/css" media="all" />
 <?php }
-add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' ); }
+?>
