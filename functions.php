9d9b7edf770ba92d6be7babe1c8af8bfb2f20cbe
Byron <contact@byronlevene.net>
2013-05-13 15:26:17 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 
 function my_login_stylesheet() { ?>
     <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo( 'stylesheet_directory' ) . '/style-login.css'; ?>" type="text/css" media="all" />
-
-<?php }
+}
+<?php {
     add_action( 'login_enqueue_scripts', 'my_login_stylesheet' ); }
 ?>
